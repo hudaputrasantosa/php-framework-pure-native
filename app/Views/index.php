7092661a1a486880fc2443ff86d00196cc078f75
Huda Putra Santosa <hudaputrasantosa@gmail.com>
2022-05-23 16:51:00 +0700

    untuk akses file public page

---
+++ app/Views/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="../public/css/bootstrap.min.css">
+   <link rel="stylesheet" href="../public/css/heroes.css">
+   <title>Php framework</title>
+</head>
+
+<body>
+   <div class="px-4 py-5 my-5 text-center">
+      <img class="d-block mx-auto mb-4" src="../public/assets/img/framework-native.png" alt="" width="270">
+      <h1 class="display-5 fw-bold">Php Framework Pure Native</h1>
+      <div class="col-lg-6 mx-auto">
+         <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Download</button>
+         </div>
+      </div>
+   </div>
+
+
+   <script src="../public/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
