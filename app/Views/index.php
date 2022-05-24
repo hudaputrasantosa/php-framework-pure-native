2217520c8e09af65ed5caa19aeaf91aca3d592fb
Huda Putra Santosa <hudaputrasantosa@gmail.com>
2022-05-24 10:22:53 +0700

    update deskripsi framework

--- app/Views/index.php
+++ app/Views/index.php
@@ -14,10 +14,10 @@
    <div class="px-4 py-5 my-5 text-center">
       <img class="d-block mx-auto mb-4" src="../public/assets/img/framework-native.png" alt="" width="270">
       <h1 class="display-5 fw-bold">Php Framework Pure Native</h1>
-      <div class="col-lg-6 mx-auto">
-         <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+      <div class="col-lg-7 mx-auto">
+         <p class="lead mb-4">Php Framework Pure Native merupakan sebuah Mini Framework yang basisnya dari bahasa pemrograman PHP Native yang dibuat struktur modelnya menggunakan model MVC (Model View Controller) untuk memudahkan pengguna untuk mengontrol sebuah metode dalam pengerjaan project. Keprivasian file Database dll. tidak bisa diakses oleh publik</p>
          <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Download</button>
+            <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Download Framework</button>
          </div>
       </div>
    </div>
