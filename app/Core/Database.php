d845acea64f07a13f75d87fcaa6c3054df15377a
Huda Putra Santosa <hudaputrasantosa@gmail.com>
2022-05-25 22:51:54 +0700

    update [database struktur]

---
+++ app/Core/Database.php
@@ -0,0 +1,30 @@
+<?php
+class Database
+{
+
+   private static $_instance = null;
+   private $mysqli;
+
+   public function __construct()
+   {
+      $this->mysqli = new mysqli('localhost', 'root', '', '') or die('Koneksi error');
+   }
+
+   public static function getInstance()
+   {
+      if (!isset(self::$_instance)) {
+         self::$_instance = new Database();
+      }
+      return self::$_instance;
+   }
+
+   public function index($table)
+   {
+      $reply = [];
+      $query = "SELECT * FROM $table";
+      $result = $this->mysqli->query($query);
+      while ($row = $result->fetch_assoc())
+         $reply[] = $row;
+      return $reply;
+   }
+}
